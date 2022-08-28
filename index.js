dbc4015e69f45c17d5d0449aead0068ff12674d2
Bikash_Gyawali <100740524+BikasGyawali@users.noreply.github.com>
2022-08-28 10:02:52 +0545

    tesst

--- index.js
+++ index.js
@@ -19,12 +19,7 @@ app.use("/api", itemRoutes);
 app.use("/api", orderRoutes);
 app.use("/api", paymentRoutes);
 
-// if(process.env.NODE_ENV === 'development') {
-//     app.use(express.static('client/build'));
-//     app.get('*', (req, res) => {
-//       res.sendFile(path.resolve(__dirname,'client','build','index.html'));
-//     });
-// }
+
  
 mongoose
   .connect(process.env.dbURI, {
