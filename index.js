9904a5a84ad1526ec49708d11e6a5afd9d9a75ae
Bikash_Gyawali <100740524+BikasGyawali@users.noreply.github.com>
2022-08-28 09:13:04 +0545

    hello

--- index.js
+++ index.js
@@ -33,6 +33,6 @@ mongoose
     useUnifiedTopology: true,
   })
   .then(() =>
-    app.listen(process.env.PORT || 4000, () => console.log(`Server running on http://localhost:${port}`))
+    app.listen(process.env.PORT || 4000, () => console.log(`Server running on http://localhost:${process.env.PORT || 4000}`))
   )
   .catch((err) => err);
