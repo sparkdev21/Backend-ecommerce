d1253b1d8c29399ac7ac48d1c0c6ed4ed5562ffc
Bikash_Gyawali <100740524+BikasGyawali@users.noreply.github.com>
2022-08-28 08:18:40 +0545

    testing

--- index.js
+++ index.js
@@ -34,6 +34,6 @@ mongoose
     useUnifiedTopology: true,
   })
   .then(() =>
-    app.listen(port, () => `Server running on http://localhost:${port}`)
+    app.listen(port, () => console.log(`Server running on http://localhost:${port}`))
   )
   .catch((err) => err);
